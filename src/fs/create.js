fb37c2e8ce0fe7aa2ba785bcfb65ebc79285729d
Chizhov Maxim <chizhov98ip@gmail.com>
2023-06-13 22:41:47 +0300

    feat:add create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const errorMsg = "FS operation failed";
+  const fileText = "I am fresh and young";
+  const filePath = "src/fs/files/fresh.txt";
+  await fs.access(filePath, async (err) => {
+    if (err) {
+      await fs.writeFile(filePath, fileText, (err) => {
+        if (err) throw new Error(errorMsg);
+      });
+    } else {
+      throw new Error(errorMsg);
+    }
+  });
 };
 
 await create();
